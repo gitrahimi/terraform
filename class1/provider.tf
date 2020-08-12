998cb5a4b8858e053d2e1a2bac076a4e98cf5f92
gitrahimi <mrahimi124@gmail.com>
2020-08-12 22:25:28 +0000

    aa

--- class1/provider.tf
+++ class1/provider.tf
@@ -2,31 +2,40 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
 resource "aws_iam_group" "developers" {
-    name = "developers"
-    path ="/"
+  name = "developers"
+  path = "/"
 }
 
 resource "aws_iam_group" "management" {
-    name = "Management"
-    path ="/"
+  name = "Management"
+  path = "/"
 }
 
 resource "aws_iam_user" "tim" {
-    name = "tim"
+  name = "tim"
 }
 
 resource "aws_iam_user" "bob" {
-    name = "bob"
+  name = "bob"
 }
+
 resource "aws_iam_user" "ben" {
-    name = "ben"
+  name = "ben"
 }
 
+resource "aws_iam_group_membership" "developers_team" {
+  name = "developers-group-membership"
 
-resource "iam_group_membership" "developers_team" {
-    name = "developers-group-membership"
+  users = ["${aws_iam_user.tim.name}", "${aws_iam_user.bob.name}", "${aws_iam_user.ben.name}"]
 
-    users =["${aws_iam_user.tim.name}"]
+  group = "${aws_iam_group.developers.name}"
+}
+
+resource "aws_iam_group_membership" "Management" {
+  name = "Management-group-membership"
+
+  users = [
+    "${aws_iam_user.bob.name}",
+  ]
 }
